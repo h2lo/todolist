6892d291b5c7b5a4ff1f94873e37a6d3e82b3eb9
h2lo <olusha.98@mail.ru>
2023-03-28 22:49:43 +0300

    Update test

--- src/state/tests/tasks-reducer.test.ts
+++ src/state/tests/tasks-reducer.test.ts
@@ -5,22 +5,41 @@ import {
     changeTaskTitleAC,
     removeTaskAC,
     tasksReducer,
-} from './tasks-reducer'
-import {addTodolistAC, removeTodolistAC} from './todolists-reducer';
+} from '../reducers/tasks-reducer'
+import {addTodolistAC, removeTodolistAC} from '../reducers/todolists-reducer';
+import {TaskPriorities, TaskStatuses} from '../../api/todolists-api';
 
 let startState: TasksStateType
 
 beforeEach(() => {
         startState = {
             'todolistId1': [
-                {id: '1', title: 'CSS', isDone: false},
-                {id: '2', title: 'JS', isDone: true},
-                {id: '3', title: 'React', isDone: false}
+                {
+                    id: '1', title: 'CSS', status: TaskStatuses.New, todoListId: 'todolistId1', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                },
+                {
+                    id: '2', title: 'JS', status: TaskStatuses.Completed, todoListId: 'todolistId1', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                },
+                {
+                    id: '3', title: 'React', status: TaskStatuses.New, todoListId: 'todolistId1', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                }
             ],
             'todolistId2': [
-                {id: '1', title: 'bread', isDone: false},
-                {id: '2', title: 'milk', isDone: true},
-                {id: '3', title: 'tea', isDone: false}
+                {
+                    id: '1', title: 'bread', status: TaskStatuses.New, todoListId: 'todolistId2', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                },
+                {
+                    id: '2', title: 'milk', status: TaskStatuses.Completed, todoListId: 'todolistId2', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                },
+                {
+                    id: '3', title: 'tea', status: TaskStatuses.New, todoListId: 'todolistId2', description: '',
+                    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+                }
             ]
         }
     }
@@ -37,7 +56,21 @@ test('correct task should be deleted from correct array', () => {
 });
 
 test('correct task should be added to correct array', () => {
-    const action = addTaskAC('juice', 'todolistId2');
+
+    const newTask = {
+        id: 'id',
+        title: 'juice',
+        status: TaskStatuses.New,
+        todoListId: 'todolistId2',
+        description: '',
+        startDate: '',
+        deadline: '',
+        addedDate: '',
+        order: 0,
+        priority: TaskPriorities.Low
+    }
+
+    const action = addTaskAC(newTask);
 
     const endState = tasksReducer(startState, action)
 
@@ -45,6 +78,8 @@ test('correct task should be added to correct array', () => {
     expect(endState['todolistId2'].length).toBe(4);
     expect(endState['todolistId2'][0].id).toBeDefined();
     expect(endState['todolistId2'][0].title).toBe('juice');
+    expect(endState['todolistId2'][0].status).toBe(TaskStatuses.New);
+
 })
 
 test('status of specified task should be changed', () => {
@@ -52,8 +87,8 @@ test('status of specified task should be changed', () => {
 
     const endState = tasksReducer(startState, action)
 
-    expect(endState['todolistId2'][1].isDone).toBe(false)
-    expect(endState['todolistId1'][1].isDone).toBe(true)
+    expect(endState['todolistId2'][1].status).toBe(TaskStatuses.Completed)
+    expect(endState['todolistId1'][1].status).toBe(TaskStatuses.New)
 })
 
 test('title of specified task should be changed', () => {
