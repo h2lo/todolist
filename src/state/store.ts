e1cb56eee6f794be8ddc1f11f228d11a093bbbf2
h2lo <olusha.98@mail.ru>
2023-03-28 17:10:50 +0300

    Build custom useAppDispatch hook, apply a middleware

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,20 @@
-import {combineReducers, legacy_createStore} from 'redux'
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
 import {tasksReducer} from './reducers/tasks-reducer';
 import {todolistsReducer} from './reducers/todolists-reducer';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {useDispatch} from 'react-redux';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppThunkDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 
 // @ts-ignore
 window.store = store
