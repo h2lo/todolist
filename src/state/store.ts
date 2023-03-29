c35c84b64974db2f5e64ed8a4a97022c4538cf09
h2lo <olusha.98@mail.ru>
2023-03-29 12:57:45 +0300

    Build custom useAppSelector hook

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,7 +2,7 @@ import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'r
 import {tasksReducer} from './reducers/tasks-reducer';
 import {todolistsReducer} from './reducers/todolists-reducer';
 import thunk, {ThunkDispatch} from 'redux-thunk';
-import {useDispatch} from 'react-redux';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
@@ -15,6 +15,7 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // @ts-ignore
 window.store = store
