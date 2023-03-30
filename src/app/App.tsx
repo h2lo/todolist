6b17f386a143652e130a2f87a6e8a0dfd7d8563a
h2lo <olusha.98@mail.ru>
2023-03-30 18:06:49 +0300

    Implement ErrorSnackBar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import BasicAppBar from '../components/BasicAppBar/BasicAppBar';
+import {BasicAppBar} from '../components/BasicAppBar/BasicAppBar';
 import Container from '@mui/material/Container';
 import TodolistsList from '../features/TodolistsList/TodolistsList';
 import {TaskType} from '../api/todolists-api';
-
+import {ErrorSnackBar} from '../components/ErrorSnackBar/ErrorSnackBar';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 export type TasksStateType = {
@@ -15,6 +15,7 @@ function App() {
 
     return (
         <div className="App">
+            <ErrorSnackBar/>
             <BasicAppBar/>
             <Container fixed>
                 <TodolistsList/>
