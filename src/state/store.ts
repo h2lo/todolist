fff15a04708f4e714a686f71c517908873703797
h2lo <olusha.98@mail.ru>
2023-03-31 21:29:50 +0300

    Add authReducer to state

--- src/state/store.ts
+++ src/state/store.ts
@@ -4,18 +4,20 @@ import {TodolistsActionsType, todolistsReducer} from './reducers/todolists-reduc
 import thunk, {ThunkDispatch} from 'redux-thunk';
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {AppActionsType, appReducer} from './reducers/app-reducer';
+import {AuthActionsType, authReducer} from './reducers/auth-reducer';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
-    app: appReducer
+    app: appReducer,
+    auth: authReducer
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, unknown, ActionsType>
-export type ActionsType = TodolistsActionsType | TasksActionsType | AppActionsType
+export type ActionsType = TodolistsActionsType | TasksActionsType | AppActionsType | AuthActionsType
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
