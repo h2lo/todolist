652ead579e9541782af371630f39f104e51a0f4d
h2lo <olusha.98@mail.ru>
2023-03-31 20:15:26 +0300

    Implement routing in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,8 @@ import Container from '@mui/material/Container';
 import TodolistsList from '../features/TodolistsList/TodolistsList';
 import {TaskType} from '../api/todolists-api';
 import {ErrorSnackBar} from '../components/ErrorSnackBar/ErrorSnackBar';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Login} from '../features/Login/Login';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 export type TasksStateType = {
@@ -18,7 +20,12 @@ function App() {
             <ErrorSnackBar/>
             <BasicAppBar/>
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList/>}></Route>
+                    <Route path={'/login'} element={<Login/>}></Route>
+                    <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to="/404"/>}/>
+                </Routes>
             </Container>
         </div>
     );
