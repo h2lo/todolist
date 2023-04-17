d7216c1fe587fdf9d539afec1abf8c8658c65c38
h2lo <olusha.98@mail.ru>
2023-04-17 14:51:08 +0300

    Rewrite store with redux-toolkit's configureStore

---
+++ src/app/store.ts
@@ -0,0 +1,25 @@
+import {AnyAction, combineReducers} from 'redux'
+import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
+import {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {appReducer} from 'app/app-reducer';
+import {authReducer} from 'features/auh/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer
+})
+
+export const store = configureStore({
+    reducer:rootReducer
+})
+
+export type AppRootStateType = ReturnType<typeof store.getState>
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<void, AppRootStateType, unknown, AnyAction>
+
+// @ts-ignore
+window.store = store
